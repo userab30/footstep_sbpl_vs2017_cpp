58c07004d1326125131c55bcf9a9e8c9c0ea071d
jiao <renjiao.jiao@qq.com>
2018-12-05 20:31:41 +0800

    获取环境EnvironmentParams参数

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,5 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include<opencv2/highgui.hpp>
-#include<opencv2/core/core.hpp>
 
 using namespace cv;
 using namespace std;
@@ -11,10 +9,6 @@ struct environment_params
 	//std::vector<Footstep> footstep_set;
 	//boost::shared_ptr<Heuristic> heuristic;
 
-	std::string heuristic_type;
-	std::string planner_type;
-	bool search_until_first_solution;
-	
 	/// Defines the area of performable (discrete) steps.
 	std::vector<std::pair<int, int> > step_range;
 
@@ -23,7 +17,7 @@ struct environment_params
 	double footsize_x, footsize_y, footsize_z;
 	double foot_origin_shift_x, foot_origin_shift_y;
 	double max_footstep_x, max_footstep_y, max_footstep_theta;
-	double max_inverse_footstep_x, max_inverse_footstep_y,max_inverse_footstep_theta;
+	double max_inverse_footstep_x, max_inverse_footstep_y, max_inverse_footstep_theta;
 	double step_cost;
 	int    collision_check_accuracy;
 	int    hash_table_size;
@@ -36,24 +30,72 @@ struct environment_params
 	double heuristic_scale;
 };
 
+int getFileParam(environment_params &ivEnvironmentParams, std::string yamlPath, std::string fileName)
+{
+
+	std::string heuristic_type;
+	double diff_angle_cost;
+
+	cv::FileStorage fs_param;
+	fs_param.open(yamlPath + fileName, cv::FileStorage::READ);
+	if (!fs_param.isOpened())
+	{
+		std::cout << fileName << ": No file!" << std::endl;
+		return -1;
+	}
+
+
+	heuristic_type = fs_param["heuristic_type"];
+	ivEnvironmentParams.heuristic_scale = 1;// fs_param["heuristic_scale"];
+	ivEnvironmentParams.hash_table_size = fs_param["max_hash_size"];
+	ivEnvironmentParams.collision_check_accuracy = fs_param["accuracy"]["collision_check"];
+	ivEnvironmentParams.cell_size = fs_param["accuracy"]["cell_size"];
+	ivEnvironmentParams.num_angle_bins = fs_param["accuracy"]["num_angle_bins"];
+
+	ivEnvironmentParams.step_cost = fs_param["step_cost"];
+	diff_angle_cost = fs_param["diff_angle_cost"];
+
+	//ivPlannerType = fs_param["planner_type"];
+	//ivSearchUntilFirstSolution = fs_param["search_until_first_solution"];
+	//ivMaxSearchTime = fs_param["allocated_time"];
+	ivEnvironmentParams.forward_search = fs_param["forward_search"] == "True";
+	//ivInitialEpsilon = fs_param["initial_epsilon"];
+	//ivChangedCellsLimit = fs_param["changed_cells_limit"];
+	ivEnvironmentParams.num_random_nodes = 20;// fs_param["num_random_nodes"];
+	ivEnvironmentParams.random_node_distance = 1.0;// fs_param["random_node_dist"];
+
+	// footstep settings
+	ivEnvironmentParams.footsize_x = fs_param["foot"]["size"]["x"];
+	ivEnvironmentParams.footsize_y = fs_param["foot"]["size"]["y"];
+	ivEnvironmentParams.footsize_z = fs_param["foot"]["size"]["z"];
+	//ivFootSeparation = fs_param["foot"]["separation"];
+	ivEnvironmentParams.foot_origin_shift_x = fs_param["foot"]["origin_shift"]["x"];
+	ivEnvironmentParams.foot_origin_shift_y = fs_param["foot"]["origin_shift"]["y"];
+	ivEnvironmentParams.max_footstep_x = fs_param["foot"]["max"]["step"]["x"];
+	ivEnvironmentParams.max_footstep_y = fs_param["foot"]["max"]["step"]["y"];
+	ivEnvironmentParams.max_footstep_theta = fs_param["foot"]["max"]["step"]["theta"];
+	ivEnvironmentParams.max_inverse_footstep_x = fs_param["foot"]["max"]["inverse"]["step"]["x"];
+	ivEnvironmentParams.max_inverse_footstep_y = fs_param["foot"]["max"]["inverse"]["step"]["y"];
+	ivEnvironmentParams.max_inverse_footstep_theta = fs_param["foot"]["max"]["inverse"]["step"]["theta"];
+	fs_param.release();
+	return 1;
+}
+
 int main(int argc, char** argv)
 {
 	string yamlPath = "..\\footstep_sbpl_vs2017_cpp\\humanoid_navigation\\footstep_planner\\config\\";
-	string fileName = "planning_params.yaml";
+	string fileName = "planning_params_all.yaml";
+	environment_params ivEnvironmentParams;
 
-	FileStorage fs;
-	fs.open(yamlPath + fileName, FileStorage::READ);
-	if (!fs.isOpened())
-	{
-		cout << "No file!" << endl;
-		return -1;
-	}
-	environment_params envParams;
-	envParams.heuristic_type = fs["heuristic_type"];
-	envParams.planner_type = fs["planner_type"];
-	envParams.search_until_first_solution = (string)fs["search_until_first_solution"] == "True";
-	cout<<envParams.planner_type;
+	getFileParam(ivEnvironmentParams, yamlPath, fileName);
 
-	fs.release();
+	std::cout << "ivEnvironmentParams.num_angle_bins:" << ivEnvironmentParams.num_angle_bins << endl;
+	std::cout << "ivEnvironmentParams.forward_search: " << ivEnvironmentParams.forward_search << std::endl;
+	std::cout << "ivEnvironmentParams.cell_size: " << ivEnvironmentParams.cell_size << std::endl;
+
+	string envPath = "..\\document\\";
+	string mapName = "env_gif.yaml";
+	string startName = "env_start.yaml";
+	string goalName = "env_goal.yaml";
 	return 0;
 }
